6214bb68c6fe787413c6d88f18226c2c6a6247df
tatsuya ogawa <togawa83@gmail.com>
2023-03-21 09:06:09 +0900

    xcframework

--- build_framework.py
+++ build_framework.py
@@ -6,60 +6,37 @@ import shutil
 import glob
 
 framework_name = "xatlas"
+project_root_dir = os.path.dirname(__file__)
 
-
-def build(srcroot, buildroot, target, arch):
-    builddir = os.path.join(buildroot, target + '-' + arch)
+def build(srcroot, buildroot, target):
+    builddir = os.path.join(buildroot, target)
     if os.path.isdir(builddir):
         shutil.rmtree(builddir)
 
     os.makedirs(builddir)
     currdir = os.getcwd()
     os.chdir(builddir)
-    cmakeargs = ("-DARCH=%s " +
-                 "-DPLATFORM=%s ") % (arch, target)
+    cmakeargs = f"-G Xcode -DCMAKE_TOOLCHAIN_FILE=ios.toolchain.cmake -DPLATFORM={target}"
     os.system("cmake %s %s" % (cmakeargs, srcroot))
-    os.system("make VERBOSE=1")
+    os.system("cmake --build . --config Release")
     os.chdir(currdir)
 
-
 def build_framework(srcroot, dstroot):
-    targets = ["iPhoneOS", "iPhoneSimulator"]
-    archs = ["arm64",  "i386"]
+    targets = ["OS","SIMULATOR"]
     for i in range(len(targets)):
-        build(srcroot, os.path.join(dstroot, "build"), targets[i], archs[i])
-    put_framework_together(srcroot, dstroot)
+        build(srcroot, os.path.join(dstroot, "build"), targets[i])
+        builddir = os.path.join(dstroot,"build",targets[i])
+    put_xcframework_together(dstroot)
 
+def put_xcframework_together(dstroot):
+    os.makedirs(f"{dstroot}/headers",exist_ok=True)
+    headers =  glob.glob(f"{project_root_dir}/source/{framework_name}/*.h",recursive=True)
+    for h in headers:
+        shutil.copy(h , f"{dstroot}/headers" )
+    libs = glob.glob(f"{dstroot}/**/lib{framework_name}.a",recursive=True)
+    libs = [f"-library {l} -headers {dstroot}/headers" for l in libs]
 
-def put_framework_together(srcroot, dstroot):
-    targetlist = glob.glob(os.path.join(dstroot, "build", "*"))
-    targetlist = [os.path.basename(t) for t in targetlist]
-
-    # set the current dir to the dst root
-    currdir = os.getcwd()
-    framework_dir = dstroot + f"/{framework_name}.framework"
-    if os.path.isdir(framework_dir):
-        shutil.rmtree(framework_dir)
-    os.makedirs(framework_dir)
-    os.chdir(framework_dir)
-
-    dstdir = "Versions/A"
-    os.makedirs(dstdir + "/Resources")
-
-    # copy headers
-    # shutil.copytree(f"../../{framework_name}", dstdir + "/Headers")
-
-    # make universal lib
-    wlist = " ".join(
-        ["../build/" + t + f"/lib{framework_name}.a" for t in targetlist])
-    os.system("lipo -create " + wlist + " -o " + dstdir + f"/{framework_name}")
-
-    # make symbolic links
-    os.symlink("A", "Versions/Current")
-    os.symlink("Versions/Current/Headers", "Headers")
-    os.symlink("Versions/Current/Resources", "Resources")
-    os.symlink(f"Versions/Current/{framework_name}", framework_name)
-
+    os.system(f"xcodebuild -create-xcframework "+" ".join(libs) + f" -output {dstroot}/{framework_name}.xcframework")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
